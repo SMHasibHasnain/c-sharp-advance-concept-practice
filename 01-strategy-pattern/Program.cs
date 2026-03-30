f917fcb9be7786deb31fc5c8649174e5ea88a21b
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-03-30 23:39:23 +0600

    Implemented dictionary-based strategy pattern for filtering numbers.

--- 01-strategy-pattern/Program.cs
+++ 01-strategy-pattern/Program.cs
@@ -3,36 +3,29 @@
     2, 3, 10, 32, 12, 44, 1, -5, -33, 22, 55, -9, 99, 66
 };
 
-
+var dictionary = new Dictionary<string, Func<int, bool>>
+{
+    ["even"] = num => num % 2 == 0,
+    ["odd"] = num => num % 2 != 0,
+    ["positive"] = num => num > 0,
+    ["negative"] = num => num < 0
+};
 
 var choice = Console.ReadLine();
 List<int> result;
-switch(choice)
-{
-    case "even" :
-        result = FilterBy(list, num => num % 2 == 0);
-        break;
-    case "odd" :
-        result = FilterBy(list, num => num % 2 != 0);
-        break;
-    case "possive" :
-        result = FilterBy(list, num => num > 0);
-        break;
-    case "negative" :
-        result = FilterBy(list, num => num < 0);
-        break;
-    default:
-        result = null!;
-        Console.WriteLine("Wrong Input!");
-        break;                      
-}
 
+if(dictionary.ContainsKey(choice!))
+{
+    result = FilterBy(list, dictionary[choice!]);
+} else
+{
+    throw new InvalidOperationException();
+}
 
 foreach(var item in result)
 {
     Console.Write(item + " ");
 }
-
 
 List<int> FilterBy(List<int> list, Func<int, bool> predicate)
 {
