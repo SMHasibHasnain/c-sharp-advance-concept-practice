5b9c77cba6a4de3fa889acd7d19ed364440d2291
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-04-16 16:50:23 +0600

    Add file handling using StreamWriter functionality.

---
+++ 03-file-handling-using-stream/Program.cs
@@ -0,0 +1,31 @@
+﻿string folder = "data";
+
+if(!Directory.Exists(folder))
+{
+    Directory.CreateDirectory(folder);
+}
+
+string filepath = Path.Combine(folder, "file");
+FileWriter writer = new(filepath);
+
+writer.Write("Hello, World!");
+writer.Write("Stream is writing!");
+writer.Write("Bye, World!");
+
+Console.ReadKey();
+
+class FileWriter
+{
+    private StreamWriter _writer;
+
+    public FileWriter(string path)
+    {
+        _writer = new StreamWriter(path, true);        
+    }
+
+    public void Write(string text)
+    {
+        _writer.WriteLine(text);
+        _writer.Flush();
+    }
+}
