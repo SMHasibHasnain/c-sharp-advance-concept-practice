fd36fb20a41b07482427500529a90137af408248
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-04-22 00:54:58 +0600

    Working With Spectre.Console to make cli better looking

---
+++ 05-better-cli-view-by-spectre/basic/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Collections;
+using Spectre.Console;
+
+AnsiConsole.MarkupLine("Welcome To [bold green]Ftruit Vallay![/]");
+
+
+CsvFileReader csvFile = new CsvFileReader();
+var data = csvFile.Read();
+
+var table = new Table();
+
+foreach(var item in data.header!)
+{
+    table.AddColumn(item);
+}
+
+
+var list = new List<string>();
+foreach (var rows in data.rows)
+{
+    list.Clear();
+    foreach (var rowData in rows)
+    {
+        list.Add(rowData);
+    }
+    table.AddRow(list.ToArray());
+}
+
+
+AnsiConsole.Write(table);
+
+class CsvFileFormat
+{
+    public string[]? header;
+    public List<string[]>? rows;
+}
+
+class CsvFileReader
+{
+    public CsvFileFormat Read()
+    {
+        StreamReader stream = new StreamReader("fruits_dataset.csv");
+
+        CsvFileFormat csvFile = new CsvFileFormat();
+
+        csvFile.header = stream.ReadLine()?.Split(",");
+
+        while(!stream.EndOfStream)
+        {
+            csvFile?.rows?.Add(stream.ReadLine()!.Split(","));
+        }
+
+        return csvFile!;
+    }
+}
