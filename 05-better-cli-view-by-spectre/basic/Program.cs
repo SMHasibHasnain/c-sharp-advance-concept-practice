2754f166bcd22db8c91b6aa09e7f22d93a93c5be
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-04-22 02:08:07 +0600

    Added CSV data runtime country filtering option

--- 05-better-cli-view-by-spectre/basic/Program.cs
+++ 05-better-cli-view-by-spectre/basic/Program.cs
@@ -3,36 +3,61 @@ using System.Collections;
 using Spectre.Console;
 
 AnsiConsole.MarkupLine("Welcome To [bold green]Ftruit Vallay![/]");
-
-
-CsvFileReader csvFile = new CsvFileReader();
-var alldata = csvFile.Read();
-
-var india_fruits = alldata.rows.Where(row => row[3] == "India").ToList();
-
-var table = new Table();
-
-foreach(var item in alldata.header!)
+bool isContinue = true;
+string choice;
+Execution exe = new();
+while(isContinue)
 {
-    table.AddColumn(item);
-}
-
-
-var list = new List<string>();
-foreach (var rows in india_fruits)
-{
-    list.Clear();
-    foreach (var rowData in rows)
+    Console.WriteLine("Enter your country: ");
+    choice = Console.ReadLine();
+    if(choice == "stop")
     {
-        list.Add(rowData);
+        isContinue = false;
+        continue;
     }
-    table.AddRow(list.ToArray());
+
+    exe.Execute(choice);
+
 }
 
 
-AnsiConsole.Write(table);
 
-Console.ReadKey();
+class Execution
+{
+    public void Execute(string choice)
+    {
+        CsvFileReader csvFile = new CsvFileReader();
+        var alldata = csvFile.Read();
+
+        var india_fruits = alldata.rows.Where(row => row[3] == choice).ToList();
+
+        var table = new Table();
+
+        foreach(var item in alldata.header!)
+        {
+            table.AddColumn(item);
+        }
+
+
+        var list = new List<string>();
+        foreach (var rows in india_fruits)
+        {
+            list.Clear();
+            foreach (var rowData in rows)
+            {
+                list.Add(rowData);
+            }
+            table.AddRow(list.ToArray());
+        }
+
+
+        AnsiConsole.Write(table);
+
+        Console.ReadKey();
+
+    }
+}
+
 
 class CsvFileFormat
 {
