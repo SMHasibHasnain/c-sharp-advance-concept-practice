25f6a62da19e87e5c3a104baa1b44c8b337431f7
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-05-06 00:22:11 +0600

    Add struct implementation for Axios included with syntax

---
+++ 10-struct/intermediate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+readonly struct Axios
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+    public int Z { get; init; }
+
+    public Axios (int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
+
+class Run
+{
+    public static void Main(String[] args)
+    {
+        Axios one = new Axios(4, 2, 6);
+        Axios two = one with { X = 5, Y = 3, Z = 7 };
+
+        Console.WriteLine("Previous: ");
+        Console.WriteLine("X: " + one.X);
+        Console.WriteLine("Y: " + one.Y);
+        Console.WriteLine("Z: " + one.Z);
+
+        Console.WriteLine("New: ");
+        Console.WriteLine("X: " + two.X);
+        Console.WriteLine("Y: " + two.Y);
+        Console.WriteLine("Z: " + two.Z);        
+    }
+}
