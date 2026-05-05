11bb71023b98d2e3f21c0e458eb1dc1cd8703d79
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-05-05 15:35:13 +0600

    Implement simple attribute of obsolete

---
+++ 09-attribute/built-in-attributes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+var task = new Task();
+task.Summery();
+task.Analysis();
+
+
+
+class Task
+{
+    public string Title { get; set; }
+    public string Cateogry { get; set; }
+    public string Description { get; set; }
+    public List<string> Tags { get; set; }
+    public DateTime DeadLine { get; set; }
+    public DateTime CreatedTime { get; init; }
+
+    [Obsolete("This method is obsoleted. Use 'Analysis Method instead!'", false)]
+    public void Summery()
+    {
+        System.Console.WriteLine("Here is task summery (old)!");
+    }
+
+    public void Analysis()
+    {
+        System.Console.WriteLine("Here is task summery (new)");
+    }
+}
