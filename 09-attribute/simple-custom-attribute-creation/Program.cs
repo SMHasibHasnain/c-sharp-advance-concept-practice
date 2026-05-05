67b9cd2c9b0d4a5c5a253fd7b2c888c2f4040d77
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-05-05 16:14:11 +0600

    Add custom attribute implementation and project files for attribute usage

---
+++ 09-attribute/simple-custom-attribute-creation/Program.cs
@@ -0,0 +1,45 @@
+﻿partial class Program
+{   
+
+    public static void Main(string[] args)
+    {
+            
+        Type personType = typeof(Person);
+        
+        object[] attributes = personType.GetCustomAttributes(typeof(DeveloperAttribute), false);
+
+        foreach (DeveloperAttribute item in attributes)
+        {
+            System.Console.WriteLine("Name: " + item.Name);
+            System.Console.WriteLine("Version: " + item.Version);
+        }
+    }
+
+    
+}
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+class DeveloperAttribute : Attribute
+{
+    public string Name { get; set; }
+    public string Version { get; set; } = "v1";
+
+    public DeveloperAttribute(string name)
+    {
+        Name = name;
+    }
+}
+
+
+[Developer("Hasib", Version = "v2")]
+class Person
+{
+    public string Name { get; set; }
+    public string Job { get; set; }
+    
+    public void DoJob()
+    {
+        System.Console.WriteLine("Doing Job...");
+    }
+}
+
