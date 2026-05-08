f26c315e43059d8f7b67f5110283104e41b1b1e4
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-05-08 23:47:07 +0600

    Add implementation for record example with equality checks

---
+++ 13-record/normal-record/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+public record Person (string Name, string Hometown);
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person1 = new Person("Hasib", "Pabna");
+        Person person2 = new Person("Mofij", "Sirajgonj");
+
+        Person person3 = new Person("Hasib", "Pabna");
+        
+        Console.WriteLine("Is person1, person2 equal? => " + person1.Equals(person2)); // False
+        Console.WriteLine("Is person1, person3 equal? => " + person1.Equals(person3)); // True
+        Console.WriteLine("Is person1, person3 equal? => " + (person1 == person3)); // True
+        
+        Console.WriteLine(person1.ToString()); //Person { Name = Hasib, Hometown = Pabna }
+
+        
+
+    } 
+}
