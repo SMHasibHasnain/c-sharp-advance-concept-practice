ee6151026ce8cc43124ce8aab7a033e75f1d5a86
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-05-10 22:18:36 +0600

    Implement HttpClient class to send http request and get response

---
+++ 15-api-basic/random-user-api/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace random_user_api_httpclient;
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using HttpClient client = new();
+        client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync("posts/1");
+            
+            // Console.WriteLine("Headers         : " + response.Headers);
+            // Console.WriteLine("StatusCode      : " + response.StatusCode);
+            // Console.WriteLine("ReasonPhrase    : " + response.ReasonPhrase);
+            // Console.WriteLine("TrailingHeaders : " + response.TrailingHeaders);
+            // Console.WriteLine("IsSucces        : " + response.IsSuccessStatusCode);
+            // Console.WriteLine("Version         : " + response.Version);
+            
+            response.EnsureSuccessStatusCode();
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonResponse);
+        } catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Message: {e.Message}");
+        }
+        
+    }
+}
